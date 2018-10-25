28b5e26ceef8379e2306954dab3a34fec90cc4dd
sergeyosipenko07 <sergey.osipenko97@gmail.com>
2018-10-25 23:21:21 +0300

    Clean Repo

--- dist/out-tsc/src/app/shared/dateFormats.js
+++
@@ -1,2 +0,0 @@
-export var DATE_FORMATS = ['YYYY/MM/DD', 'DD MMMM YYYY', 'DD-MMM-YY'];
-//# sourceMappingURL=dateFormats.js.map
--- src/app/shared/dateFormats.ts
+++
@@ -1,1 +0,0 @@
-export const DATE_FORMATS = ['YYYY/MM/DD', 'DD MMMM YYYY', 'DD-MMM-YY'];
